7e2c0b187e6a0a3008ba4ef39ea8e6511f33053e
Le Tien Tai <Tai.LeTien@vn.bosch.com>
2016-02-23 23:52:23 +0700

    Add the install_utils.sh script

---
+++ install_utils.sh
@@ -0,0 +1,14 @@
+# Here the list of the PPA
+
+
+# Don't forget to update the apt-get list to get latest version
+sudo apt-get update
+
+
+# The common utils
+#------------------
+
+# Xclip helpful to pipe from command output to clipboard
+sudo apt-get install xclip
+
+
