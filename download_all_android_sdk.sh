a856f899921605c64dff1a00646ddb9b6a8434f7
Le Tien Tai <Tai.LeTien@vn.bosch.com>
2016-03-24 05:45:28 +0700

    Remove dump code

--- download_all_android_sdk.sh
+++ download_all_android_sdk.sh
@@ -1,33 +1,28 @@
-#!/bin/bash
+#!/binbash
 # Install all non-obsolete android sdk packages.
+# author: Tai Le Tien (letientai299 at gmail.com)
 
 function install_sdk {
   android update sdk -u -s -a -t "$1"
 }
 
-
-
-# install_sdk 163
-
 function fetch_non_obsoled_package_indices {
-  # Fetch the sdk list
+  # Fetch the sdk list using non-https connections
   android list sdk -u -s -a |\
     # Filter obsoleted packages
     sed '/\(Obsolete\)/d' |\
-    # Filter to take only the package index
+    # Filter to take only the index number of package
     sed 's/^[ ]*\([0-9]*\).*/\1/' |\
-    # Filter the empty lines
+    # Remove the empty lines
     sed -n 's/^[^ $]/\0/p'
-    # Send the output to the for loop one by one
 }
-
-
 
 for package_index in  $(fetch_non_obsoled_package_indices)
 do
   echo "====================================================================="
   echo "Start to install package:  ${package_index}"
   echo "====================================================================="
+  # Auto accept license
   echo -e "y" | install_sdk "${package_index}"
   echo
   echo
