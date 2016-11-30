310cb6bfb9ea96f9f15b1b90a82ec70740b774c6
Le Tien Tai <tai.letien@vn.bosch.com>
2016-11-30 10:02:29 +0700

    Update script

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get autoremove
-sudo apt-get autoclean
-sudo apt-get update
-sudo apt-get full-upgrade -y
+sudo apt autoremove
+sudo apt autoclean
+sudo apt update
+sudo apt full-upgrade -y
