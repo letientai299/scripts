04186e38eae407ed1482020e90c26310aee2e749
Le Tien Tai <tai.letien@vn.bosch.com>
2017-02-27 16:28:59 +0700

    Vscode now be able to update using apt

--- update-vscode.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Update vscode from downloaded deb file
-url="https://go.microsoft.com/fwlink/?LinkID=760865"
-
-rm -f /tmp/vscode.deb
-curl -L $url > /tmp/vscode.deb
-sudo dpkg --install /tmp/vscode.deb
-
-exit 0
