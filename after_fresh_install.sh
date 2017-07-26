2f51a76f032581b2cb15ca74e2416b8f20abd5bb
John Lee <john299.gee@gmail.com>
2017-07-26 14:44:00 +0800

    Add script to fix NPM permissions

--- after_fresh_install.sh
+++ after_fresh_install.sh
@@ -75,6 +75,12 @@ task "Nodejs"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+task "Fix NPM permissions"
+mkdir ~/.npm-global
+npm config set prefix "$HOME/.npm-global"
+echo 'Fix npm permissions' >> $HOME/.profile
+echo 'export PATH=$PATH:$HOME/.npm-global/bin' >> $HOME/.profile
+
 task "yarn"
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
